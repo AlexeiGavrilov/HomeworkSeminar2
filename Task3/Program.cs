f0b49cc5b1c3820975c77ea04359d18367a538db
Alex <kccnbnb@mail.ru>
2022-12-06 18:33:59 +0300

    add task3

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 23 ==0)
+{
+    if (num % 7 ==0);
+    Console.WriteLine("Ваше число: " +num +" кратно и 7 и 23");
+}
+else
+{
+    Console.WriteLine("Ваше число не кратно и 7 и 23");
+}
